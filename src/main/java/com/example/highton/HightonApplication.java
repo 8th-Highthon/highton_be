6e44cee606f2a9b9d83bd9e8102d7d044d45639c
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-14 15:30:19 +0900

    project start

---
+++ src/main/java/com/example/highton/HightonApplication.java
@@ -0,0 +1,13 @@
+package com.example.highton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HightonApplication.class, args);
+    }
+
+}
