eeca6eef62c31b845765bf3d41a2299bd2d63699
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-15 01:37:07 +0900

    :sparkles: :: Login Logic

--- src/main/java/com/example/highton/domain/account/service/AccountService.java
+++ src/main/java/com/example/highton/domain/account/service/AccountService.java
@@ -2,21 +2,26 @@ package com.example.highton.domain.account.service;
 
 import com.example.highton.domain.account.Account;
 import com.example.highton.domain.account.exception.AccountNotFoundException;
+import com.example.highton.domain.account.exception.PasswordNotMatchedException;
 import com.example.highton.domain.account.presentation.dto.request.SignInRequest;
 import com.example.highton.domain.account.presentation.dto.response.SignInResponse;
 import com.example.highton.domain.account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SignInResponse signIn(SignInRequest signInRequest) {
-        Account user = accountRepository.findByUserId(signInRequest.getUserId());
-        if(user == null) {
-            throw new AccountNotFoundException();
+        System.out.println(passwordEncoder.encode(signInRequest.getPassword()));
+        Account user = accountRepository.findByUserId(signInRequest.getUserId())
+                .orElseThrow(() -> new AccountNotFoundException());
+        if(!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
+            throw new PasswordNotMatchedException();
         }
         return new SignInResponse(user.getUserId());
     }
