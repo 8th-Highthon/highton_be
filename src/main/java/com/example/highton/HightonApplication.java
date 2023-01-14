c06808fced4efa9b7bade179d948d3985551e7f6
전승원 <78971821+louis7308@users.noreply.github.com>
2023-01-14 20:41:53 +0900

    :sparkles: :: Domain , Global 패키지 구성

--- src/main/java/com/example/highton/HightonApplication.java
+++ src/main/java/com/example/highton/HightonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HightonApplication {
-
     public static void main(String[] args)   {
         SpringApplication.run(HightonApplication.class, args);
     }
