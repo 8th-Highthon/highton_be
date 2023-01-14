7322e2fa4e3a27c3960880a5676f7962ee509448
이승윤 <yyuunn17@naver.com>
2023-01-15 02:32:06 +0900

    feat : 판매 물품 상세 조회 API

--- src/main/java/com/example/highton/domain/item/presentation/ItemController.java
+++ src/main/java/com/example/highton/domain/item/presentation/ItemController.java
@@ -3,12 +3,15 @@ package com.example.highton.domain.item.presentation;
 import com.example.highton.domain.item.presentation.request.CreateItemRequest;
 import com.example.highton.domain.item.presentation.request.DeleteItemRequest;
 import com.example.highton.domain.item.presentation.response.CreateItemResponse;
+import com.example.highton.domain.item.presentation.response.FindItemDetailsResponse;
 import com.example.highton.domain.item.service.ItemService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +38,13 @@ public class ItemController {
     public void deleteItem(@RequestBody @Valid DeleteItemRequest request) {
         itemService.deleteItem(request);
     }
+
+    @Operation(summary = "판매 물품 상세 조회")
+    @GetMapping("/{item-id}")
+    public FindItemDetailsResponse findItemDetails(@PathVariable("item-id") Long itemId) {
+        return itemService.findItemDetails(itemId);
+    }
+
+
 
 }
